5949b59e2f2b23c47b82c8d3ed195c30905da12b
Gireesh Kumar <gireesh.kr@pitsolutions.com>
2024-11-08 22:33:49 +0530

    CustomExceptionHandler

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -5,6 +5,21 @@ namespace Catalog.API.Products.UpdateProduct
     public record UpdateProductCommand(Guid Id, string Name,List<string> Category,string Description,string ImageFile,decimal Price)
             :ICommand<UpdateProductResult>;
     public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Product Id is required");
+
+            RuleFor(command => command.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .Length(2, 255).WithMessage("Name must be between 2 and 255 characters");
+
+            RuleFor(command => command.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than 0");
+        }
+    }
     internal class UpdateProductCommandHandler
         (IDocumentSession session,ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
@@ -16,7 +31,7 @@ namespace Catalog.API.Products.UpdateProduct
             var product=await session.LoadAsync<Product>(command.Id,cancellationToken);
 
             if (product is null) {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(command.Id);
             }
 
             product.Name=command.Name;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,5 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services in the container
@@ -6,16 +8,22 @@ builder.Services.AddCarter();
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviors<,>));
 });
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddMarten(config =>
 {
     config.Connection(builder.Configuration.GetConnectionString("Database"));   
 }).UseLightweightSessions();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 //Configure HTTP pipelines
 
 app.MapCarter();
+
+app.UseExceptionHandler(options => { })
 
 app.Run();
