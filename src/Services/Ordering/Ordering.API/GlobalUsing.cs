b722b5a0791e8414bb4f411ab94f2af4bd3e3204
Gireesh Kumar <gireesh.kr@pitsolutions.com>
2024-12-15 23:38:28 +0530

    Ordering servcies

--- src/Services/Ordering/Ordering.API/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -2,4 +2,7 @@
 global using Ordering.Application;
 global using Ordering.Infrastructure;
 global using Ordering.Infrastructure.Data.Extensions;
-
+global using Carter;
+global using Ordering.Application.Dtos;
+global using MediatR;
+global using Mapster;
